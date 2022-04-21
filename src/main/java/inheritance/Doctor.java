93837265b0c427668d62ffa107b13668a2120dca
Cashtanov <CashtanovIS@yandex.ru>
2022-04-21 14:23:47 +0500

    Revert "3. Реализация профессий в коде [#6837 #291402]"
    This reverts commit f9f45d312a311f3e3f70bdeb0b386970160bae8d.

--- src/main/java/inheritance/Doctor.java
+++ src/main/java/inheritance/Doctor.java
@@ -4,12 +4,12 @@ public class Doctor extends Profession {
 
     private Pacient pacient;
 
-    public Doctor(String name, String surname, String education, String birthday) {
+    public Doctor(String name, String surname, String education, String birthday, Pacient pacient) {
         super(name, surname, education, birthday);
         this.pacient = pacient;
     }
 
-    public static Diagnosis heal(Pacient pacient) {
-        return new Diagnosis("Диагноз");
+    public Diagnosis heal(Pacient pacient) {
+        return new Diagnosis("jfj");
     }
 }
