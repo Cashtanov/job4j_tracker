afe087c701af2efac57ae14d25b359ffee86e764
Cashtanov <CashtanovIS@yandex.ru>
2022-04-21 14:20:02 +0500

    3. Реализация профессий в коде [#6837 #291402]

--- src/main/java/inheritance/Doctor.java
+++ src/main/java/inheritance/Doctor.java
@@ -4,12 +4,12 @@ public class Doctor extends Profession {
 
     private Pacient pacient;
 
-    public Doctor(String name, String surname, String education, String birthday, Pacient pacient) {
+    public Doctor(String name, String surname, String education, String birthday) {
         super(name, surname, education, birthday);
         this.pacient = pacient;
     }
 
-    public Diagnosis heal(Pacient pacient) {
-        return new Diagnosis("jfj");
+    public static Diagnosis heal(Pacient pacient) {
+        return new Diagnosis("Диагноз");
     }
 }
