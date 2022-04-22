a1c402a1fc9a16a0f7f6a95784f2bf3c499844f7
Cashtanov <CashtanovIS@yandex.ru>
2022-04-22 15:13:29 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Петухов Илья Станиславович");
+        student.setGroup("44p");
+        student.setReceiptDate(new Date());
+
+        System.out.println(student.getFullName() + " entered the " + student.getGroup() + " : " + student.getReceiptDate());
+    }
+}
