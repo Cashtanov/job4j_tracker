80918a396714b2a6d12275959a18cf5dd28123ab
Cashtanov <CashtanovIS@yandex.ru>
2022-04-22 13:18:40 +0500

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,10 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
@@ -37,5 +33,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
