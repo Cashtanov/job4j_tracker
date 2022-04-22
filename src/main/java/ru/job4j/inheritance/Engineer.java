2a3132545983ae6e2b4e1ad4cf8450de3a6c20e6
Cashtanov <CashtanovIS@yandex.ru>
2022-04-22 15:19:22 +0500

    3. Реализация профессий в коде [#6837 #291402]

--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class Engineer extends Profession {
     private Project project;
 
-    public Engineer(String name, String surname, String education, String birthday, Project project) {
+    public Engineer(String name, String surname, String education, String birthday) {
         super(name, surname, education, birthday);
         this.project = project;
     }
