b51b4eb0799e2feb169aaa23922c892fb3d7f277
github.com <Bkmzpett@gmsil.com>
2022-04-23 14:44:03 +0300

    3. Массивы и модели. [#174892 #292289]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -23,7 +23,7 @@ public class Library {
         }
         System.out.println("Shown only books.name > Clean code");
         for (Book bk : books) {
-            if (bk.getName().equals("Clean code")) {
+            if (("Clean code").equals (bk.getName())) {
                 System.out.println(bk.getName() + " - " + bk.getPages());
             }
         }
