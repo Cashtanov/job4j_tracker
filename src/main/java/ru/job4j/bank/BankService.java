d023c11137dcd5b742d8586fcc359104877e57e0
Cashtanov <CashtanovIS@yandex.ru>
2022-06-03 14:06:44 +0500

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,18 +9,22 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-            users.putIfAbsent(user, new ArrayList<Account>());
+        users.putIfAbsent(user, new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        if (user != null && !users.get(user).contains(account)) {
-            users.get(user).add(account);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+
+            }
         }
     }
 
     public User findByPassport(String passport) {
-        for (User user: users.keySet()) {
+        for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
                 return user;
             }
